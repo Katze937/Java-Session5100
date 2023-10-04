e6ff1ae172df2cb0cb37d36a49f4604db874ad23
Katze937 <speg2820@gmail.com>
2023-10-04 18:05:48 -0400

    MainFrameVersion1
    Only set up the panel and the structure of the pop up screen, no function nor text field can be read

--- Lab3_Assignment_YupeiChen/MainFrame/src/main/java/com/mycompany/mainframe/MainFrame.java
+++ Lab3_Assignment_YupeiChen/MainFrame/src/main/java/com/mycompany/mainframe/MainFrame.java
@@ -8,39 +8,34 @@ package com.mycompany.mainframe;
  *
  * @author isabe
  */
-public class MainFrame extends javax.swing.JFrame{
-    
-    public MainFrame(){
+public class MainFrame extends javax.swing.JFrame {
+
+    public MainFrame() {
         initComponents();
     }
-    
-    private void initComponents(){
+
+    private void initComponents() {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 400, Short.MAX_VALUE)
-    );
-    
-    layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 300, Short.MAX_VALUE)
-    );
-    pack();
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 400, Short.MAX_VALUE));
+
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 300, Short.MAX_VALUE));
+        pack();
     }
-    
-   
-    public static void main(String args[]){
-        
-        java.awt.EventQueue.invokeLater(new Runnable(){
-            public void run(){
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
                 new MainFrame().setVisible(true);
             }
         });
     }
-    
+
+    // test
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
 }
-        
-        
-   
