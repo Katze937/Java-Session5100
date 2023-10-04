9b8b42334ff61a4877178827523a2b4c9e31980b
Katze937 <speg2820@gmail.com>
2023-10-04 14:19:46 -0400

    Lab3_Assignment_Swing

---
+++ Lab3_Assignment_YupeiChen/MainFrame/src/main/java/com/mycompany/mainframe/MainFrame.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mainframe;
+
+/**
+ *
+ * @author isabe
+ */
+public class MainFrame extends javax.swing.JFrame{
+    
+    public MainFrame(){
+        initComponents();
+    }
+    
+    private void initComponents(){
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 400, Short.MAX_VALUE)
+    );
+    
+    layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 300, Short.MAX_VALUE)
+    );
+    pack();
+    }
+    
+   
+    public static void main(String args[]){
+        
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+    
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+}
+        
+        
+   
